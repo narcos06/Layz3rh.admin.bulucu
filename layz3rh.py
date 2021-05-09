921d821531062dba204ba79575c007217ec66519
narcos06 <81692411+narcos06@users.noreply.github.com>
2021-05-09 14:36:20 +0300

    Update layz3rh.py

--- layz3rh.py
+++ layz3rh.py
@@ -39,7 +39,7 @@ for kelime in kelimeler:
 ===>"+sonuc)
         
         print("================================")
-                     
+        print("                                             ")
 
                          
         with open("siteler.txt","a") as file:
